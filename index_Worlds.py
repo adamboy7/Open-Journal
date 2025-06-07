3086d63f26b99eaac21493d0530926a8f151b1c0
adamboy7 <adamboy8888@gmail.com>
2025-06-07 05:44:59 -0400

    Fix file handling in world index

--- index_Worlds.py
+++ index_Worlds.py
@@ -20,51 +20,51 @@ for filename in glob.glob(os.path.join("Worlds", "*")) :
     rewards_Temp = list()
     enemies_Temp = list()
     try:
-        file_Chests = open(os.path.join("Worlds", name_Temp, "chests.txt"), encoding='utf-8-sig')
-        line_Count = 0
-        for line in file_Chests :
-            line_Count = line_Count + 1
-            line = line.strip()
-            try:
-                line = int(line)
-                chests_Temp.append(line)
-            except:
-                print ('Invalid drop in', filename + 'chests.txt', 'Line:', line_Count)
-                continue
+        with open(os.path.join("Worlds", name_Temp, "chests.txt"), encoding='utf-8-sig') as file_Chests:
+            line_Count = 0
+            for line in file_Chests:
+                line_Count += 1
+                line = line.strip()
+                try:
+                    line = int(line)
+                    chests_Temp.append(line)
+                except Exception:
+                    print('Invalid drop in', os.path.join(filename, 'chests.txt'), 'Line:', line_Count)
+                    continue
         chests[name_Temp] = chests_Temp
-    except:
-        print ('No chests found in', name_Temp)
+    except Exception:
+        print('No chests found in', name_Temp)
         chests[name_Temp] = chests_Temp
     try:
-        file_Rewards = open(os.path.join("Worlds", name_Temp, "rewards.txt"), encoding='utf-8-sig')
-        line_Count = 0
-        for line in file_Rewards :
-            line_Count = line_Count + 1
-            line = line.strip()
-            try:
-                line = int(line)
-                rewards_Temp.append(line)
-            except:
-                print ('Invalid drop in', filename + 'rewards.txt', 'Line:', line_Count)
-                continue
+        with open(os.path.join("Worlds", name_Temp, "rewards.txt"), encoding='utf-8-sig') as file_Rewards:
+            line_Count = 0
+            for line in file_Rewards:
+                line_Count += 1
+                line = line.strip()
+                try:
+                    line = int(line)
+                    rewards_Temp.append(line)
+                except Exception:
+                    print('Invalid drop in', os.path.join(filename, 'rewards.txt'), 'Line:', line_Count)
+                    continue
         rewards[name_Temp] = rewards_Temp
-    except:
-        print ('No rewards found in', name_Temp)
+    except Exception:
+        print('No rewards found in', name_Temp)
         rewards[name_Temp] = rewards_Temp
 
     try:
-        file_Enemies = open(os.path.join("Worlds", name_Temp, "enemies.txt"), encoding='utf-8-sig')
-        line_Count = 0
-        for line in file_Enemies :
-            line_Count = line_Count + 1
-            line = line.strip()
-            try:
-                line = int(line)
-                enemies_Temp.append(line)
-            except:
-                print ('Invalid drop in', filename + 'enemies.txt', 'Line:', line_Count)
-                continue
+        with open(os.path.join("Worlds", name_Temp, "enemies.txt"), encoding='utf-8-sig') as file_Enemies:
+            line_Count = 0
+            for line in file_Enemies:
+                line_Count += 1
+                line = line.strip()
+                try:
+                    line = int(line)
+                    enemies_Temp.append(line)
+                except Exception:
+                    print('Invalid drop in', os.path.join(filename, 'enemies.txt'), 'Line:', line_Count)
+                    continue
         enemies[name_Temp] = enemies_Temp
-    except:
-        print ('No enemies found in', name_Temp)
+    except Exception:
+        print('No enemies found in', name_Temp)
         enemies[name_Temp] = enemies_Temp
